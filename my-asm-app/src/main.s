a4e328700f25acb67350c1ea140502bf4fa3a156
Mark Fisher <mark.j.fisher@gmail.com>
2023-11-09 20:50:25 +0000

    Fix apple2 asm only build, using forceimport

--- my-asm-app/src/main.s
+++ my-asm-app/src/main.s
@@ -2,9 +2,11 @@
 
     .import   _cgetc
     .import   _cputs
+    ; in a pure asm application (or where main is in asm, not sure...)
+    ; this is required for the APPLE2 target, doesn't harm the atari
+    .forceimport    __STARTUP__
 
 _main:
-
     ; write a string to the screen
     lda   #<hello_msg
     ldx   #>hello_msg
